6a953e155e5fecf25929ac7346a7d9135bcd5880
Viktor Moskalenko <tehb23@gmail.com>
2022-06-23 21:33:02 +0300

    First task complete

---
+++ homework1/task_1.py
@@ -0,0 +1,17 @@
+import re
+
+
+def domain_name(url: str)->str:
+    """Return domain name from url"""
+    match = re.search(r'[./]?(?:www.)?\w+\-?\w+[.]', url)
+    result = match[0]
+    result = result.replace('www', '')
+    result = result.replace('.', '')
+    result = result.replace('/', '')
+    return result
+
+assert domain_name("http://google.com") == "google"
+assert domain_name("http://google.co.jp") == "google"
+assert domain_name("www.xakep.ru") == "xakep"
+assert domain_name("https://youtube.com") == "youtube"
+
