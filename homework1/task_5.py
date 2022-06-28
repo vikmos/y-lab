4d2dbc4b8e10bb6805e1beb1a6efd55196bc1631
Viktor Moskalenko <tehb23@gmail.com>
2022-06-28 10:24:00 +0300

    Task 5 completed

--- homework1/task_5.py
+++ homework1/task_5.py
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+""" Написать метод count_find_num, который принимает на вход список простых 
+    множителей (primesL) и целое число, предел (limit), после чего попробуйте 
+    сгенерировать по порядку все числа. Меньшие значения предела, которые 
+    имеют все и только простые множители простых чисел primesL. """
+
+>>>>>>> f27e1c6 (Task 5 completed)
 from itertools import combinations_with_replacement as comb
 from functools import reduce
 from operator import mul
@@ -5,6 +13,7 @@ from operator import mul
 
 def count_find_num(primesL, limit):
     min_mul = reduce(mul, primesL)
+<<<<<<< HEAD
     max_mul = min_mul
     count = 1
     i = 1 
@@ -46,3 +55,26 @@ primesL = [2, 3, 47]
 limit = 200
 assert count_find_num(primesL, limit) == []
 
+=======
+    step = limit // min_mul
+    max_mul = min_mul
+    count = 1
+    i = 1
+    res = []
+    if min_mul > limit:
+        return res
+    flag = True
+    while flag:
+        flag = False
+        arr = comb(primesL, i)
+        for el in arr:
+            temp_mul = reduce(mul, el)
+            if temp_mul <= step:
+                max_mul = max(temp_mul * min_mul, max_mul)
+                count += 1
+                flag = True
+        i += 1
+    res.append(count)
+    res.append(max_mul)
+    return res
+>>>>>>> f27e1c6 (Task 5 completed)
